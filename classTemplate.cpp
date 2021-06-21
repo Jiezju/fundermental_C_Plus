77877eed3f42deb306af474131cbe8d4b84b344a
Jiezju <21725100@zju.edu.cn>
2021-06-21 23:44:22 +0800

    template

--- classTemplate.cpp
+++ classTemplate.cpp
@@ -6,37 +6,40 @@
 using namespace std;
 
 template<class T>
-class TC {
+class TC
+{
 public:
     TC(T a, T b, T c);
-
     T Min();
-
     T Max();
 
 private:
-    T m_a, m_b, m_c;
+    T m_a,m_b,m_c;
 };
 
 template<class T>
-TC<T>::TC(T a, T b, T c) : m_a{a}, m_b{b}, m_c{c} { ; }
+TC<T>::TC(T a, T b, T c) : m_a{a}, m_b {b}, m_c{c} {;}
 
 template<class T>
-T TC<T>::Min() {
+T TC<T>::Min()
+{
     T minab = m_a < m_b ? m_a : m_b;
     return minab < m_c ? minab : m_c;
 }
 
 template<class T>
-T TC<T>::Max() {
+T TC<T>::Max()
+{
     T maxab = m_a > m_b ? m_a : m_b;
     return maxab > m_c ? maxab : m_c;
 }
 
-int main() {
-    TC<int> obj1(2, 3, 4);
-    cout << obj1.Min() << endl;
-    cout << obj1.Max() << endl;
+int main()
+{
+    TC<int> obj1(2,3,4);
+
+    cout<<obj1.Min()<<endl;
+    cout<<obj1.Max()<<endl;
 
     TC<double> obj2(2.5, 4.4, 3.3);
     cout << obj2.Min() << endl;
